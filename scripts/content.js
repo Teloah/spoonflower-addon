f379f52a0167332ffbb651fe2e1bc2c790743061
Andris Klaipins <teloah@gmail.com>
2024-01-25 23:12:51 +0000

    Extracted CSS into a separate file

--- scripts/content.js
+++ scripts/content.js
@@ -3,29 +3,6 @@ setTimeout(() => {
   const imageSelector = "img.swiper-image";
   const addedClass = "spoonflower-xl";
 
-  const styleNode = document.createElement("style");
-  styleNode.innerHTML = `
-  div.swiper-slide:has(img.${addedClass})::after {
-    content: "L";
-    position: absolute;
-    bottom: 1px;
-    left: 1px;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    background: #2d6e76;
-    color: #fff;
-    width: 15px;
-    height: 15px;
-    line-height: 0.75rem;
-    font-size: 0.75rem;
-    border-radius: 50%;
-    border: 1px solid #fff;
-  }
-`;
-
-  document.body.appendChild(styleNode);
-
   const findImages = () => {
     const selector = pageClasses
       .map((c) => `article.${c} ${imageSelector}`)
