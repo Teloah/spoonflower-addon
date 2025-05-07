eb8b54f18b66b68dbd845810ef35ac3684fcb148
Andris Klaipins <teloah@gmail.com>
2025-05-07 15:19:38 +0100

    Support for s images; formatter

--- scripts/content.js
+++ scripts/content.js
@@ -1,53 +1,56 @@
 setTimeout(() => {
-  const pageClasses = ["x-wallpaper", "x-fabric", "x-home-good"];
-  const imageSelector = "img.swiper-image";
-  const addedClass = "spoonflower-xl";
+  const pageClasses = ['x-wallpaper', 'x-fabric', 'x-home-good']
+  const imageSelector = 'img.swiper-image'
+  const addedClass = 'spoonflower-xl'
 
   const updateImage = (img) => {
-    img.src = img.src.replace("/xs/", "/l/").replace("/m/", "/l/");
-    img.classList.add(addedClass);
-  };
+    img.src = img.src
+      .replace('/xs/', '/l/')
+      .replace('/s/', '/l/')
+      .replace('/m/', '/l/')
+    img.classList.add(addedClass)
+  }
 
   const updateLazyImages = () => {
     // updates lazy loaded images in the products overview, for example
     // https://www.spoonflower.com/en/products/16981942-party-style-m-floral-spinning-wheel-fireworks-yellow-on-dark-eggplant-violet-background-by-klaipina?product=homegoods-kitchen-dining
-    const parentDiv = document.querySelector(".b-products-overview");
+    const parentDiv = document.querySelector('.b-products-overview')
 
     if (!parentDiv) {
-      return;
+      return
     }
 
-    const config = { attributes: false, childList: true, subtree: true };
+    const config = { attributes: false, childList: true, subtree: true }
 
     const callback = (mutationList) => {
       for (const mutation of mutationList) {
-        if (mutation.type === "childList") {
-          const nodes = mutation.addedNodes;
+        if (mutation.type === 'childList') {
+          const nodes = mutation.addedNodes
           for (const node of nodes) {
-            if (node.nodeName === "IMG" && node.classList.contains("image")) {
-              updateImage(node);
+            if (node.nodeName === 'IMG' && node.classList.contains('image')) {
+              updateImage(node)
             }
           }
         }
       }
-    };
+    }
 
-    const observer = new MutationObserver(callback);
-    observer.observe(parentDiv, config);
-  };
+    const observer = new MutationObserver(callback)
+    observer.observe(parentDiv, config)
+  }
 
   const updateImmediateImages = () => {
     // updates images that are immediately loaded on the page, for example
     // https://www.spoonflower.com/en/fabric/16981942-party-style-m-floral-spinning-wheel-fireworks-yellow-on-dark-eggplant-violet-background-by-klaipina
-    const productsSelectors = [".b-products-overview img.image"];
-    const designsSelectors = pageClasses.map((c) => `.${c} ${imageSelector}`);
+    const productsSelectors = ['.b-products-overview img.image']
+    const designsSelectors = pageClasses.map((c) => `.${c} ${imageSelector}`)
 
-    const selector = productsSelectors.concat(designsSelectors).join(", ");
+    const selector = productsSelectors.concat(designsSelectors).join(', ')
 
-    const images = document.querySelectorAll(selector);
-    images.forEach(updateImage);
-  };
+    const images = document.querySelectorAll(selector)
+    images.forEach(updateImage)
+  }
 
-  updateImmediateImages();
-  updateLazyImages();
-}, 2000);
+  updateImmediateImages()
+  updateLazyImages()
+}, 2000)
